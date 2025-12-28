59baeb6a62b2a3dc6053be96603e8ef941e5179b
Kapilan <sriranjankapilan@gmail.com>
2025-12-28 15:49:40 +0530

    improved spaCy model loading and expand skill detection in resume analysis

--- utils/analyse_resume.py
+++ utils/analyse_resume.py
@@ -1,17 +1,23 @@
 import re
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def analyse_resume(text):
     doc = nlp(text)
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
-    
-    # Predefined skill pattern
-    skills = re.findall(r'\b(Python|Java|SQL|Excel|C\+\+|TensorFlow|AWS|Docker|JavaScript)\b', text, re.I)
-    
+
+    skills = re.findall(
+        r'\b(Python|Java|SQL|Excel|C\+\+|TensorFlow|AWS|Docker|JavaScript|React|Django|Flask)\b',
+        text,
+        re.I
+    )
+
     return {
         "word_count": len(text.split()),
-        "entities_found": entities[:50],  # Limit to first 50 for display
         "skills_found": list(set(skills))
     }
